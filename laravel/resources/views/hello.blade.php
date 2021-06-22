823e0d0fe92fa1b7f88cb8ddc5452b50775952db
Baptiste Martin <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-06-22 16:25:16 +0200

    Etape 1
    Réussie

---
+++ laravel/resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body style="background-color: black;">
+    <h1 style="color: white; font-family: sans-serif; text-align: center;">Hello World From Laravel v8.X (PHP v7.4.1)</h1>
+  </body>
+</html>
