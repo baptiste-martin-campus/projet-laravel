d32f2a36419725a382e0bc0632befc41d2204d4c
baptiste-martin-campus <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-06-23 16:55:01 +0200

    MAJ site biere

--- laravel/resources/views/homepage.blade.php
+++ laravel/resources/views/homepage.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Home page</title>
-  </head>
-  <body style="background-color: black;">
-    <h1 style="color: white; text-align: center; font-family:sans-serif; border: 1px solid white;">Home page</h1>
-  </body>
-</html>
+@extends('menus')
+@section('content')
+    <h1 style="text-align: center; font-family:sans-serif; border: 1px solid white;">Home page</h1>
+
+@endsection
