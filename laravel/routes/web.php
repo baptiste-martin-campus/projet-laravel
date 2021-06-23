4a901d645abf5a0443151c1222f8a8535b07008f
baptiste-martin-campus <81614331+baptiste-martin-campus@users.noreply.github.com>
2021-06-23 10:50:43 +0200

    Iteration 2
    Etape 1 réussie

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,5 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('hello');
+    return "Home page";
+
+});
+
+Route::get('/product', function () {
+    return "Liste des produits";
+
+});
+
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit" ." ". $id;
+
+});
+
+
+Route::get('/cart', function () {
+    return "Panier";
+
 });
